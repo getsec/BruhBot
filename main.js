59e53189eff8d19f77c64abdb85ee28e5bf1fdfe
nathan getty <nathan.getty@skipthedishes.ca>
2020-06-16 19:14:55 -0500

    push

--- main.js
+++ main.js
@@ -5,13 +5,35 @@ const client = new Discord.Client();
 var express = require("express");
 var app = express();
 var inquirer = require('inquirer');
-const ping = require('./commands/ping')
+const ping = require('./commands/ping') 
 
 require('dotenv').config(); 
 
 const prefix = process.env.PREFIX;
 const token  = process.env.CLIENT_TOKEN;
 
+/**
+ * Plays a sond on the proper dispatcher.
+ * @param {*} file
+ * @param {*} connection
+ * @param {*} dispatcher
+ */
+const playSound = async (file, connection, dispatcher) => {
+
+    // play
+    dispatcher.on('start', () => {
+        console.log(`${file} is now playing!`);
+    });
+    
+    // when finished. Close 
+    dispatcher.on('finish', () => {
+        console.log(`${file} has finished playing!`);
+        connection.disconnect();    
+    });
+
+    // Always remember to handle errors appropriately!
+    dispatcher.on('error', console.error);
+  }
 
 
 // When the application starts, this is required...
@@ -32,7 +54,8 @@ client.on('message', message => {
         message.channel.send(args)
 	} else if (command === 'beep') {
 		message.channel.send('Boop.');
-	}
+    }
+
     // other commands...
 });
 
@@ -48,26 +71,40 @@ client.on('message' , message => {
 })
 
 
+// Curb your enthusiasm
+client.on('message', async message =>{
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+	const args = message.content.slice(prefix.length).split(/ +/);
+	const command = args.shift().toLowerCase();
+    if (command == "curb"){
+        const file = 'assets/curb-your-enthusiasm.mp3';
+        
+        try {
+            const connection = await message.member.voice.channel.join();
+            const dispatcher = connection.play(file);
+            playSound(file, connection, dispatcher) 
+        } catch (e){
+            message.channel.send('Bruh - to play a sound you need to be in a channel...');
+        }
+    }
+    if (command == "kill"){
+        message.member.voice.channel.leave()
+        // const connection = await message.member.voice.channel.join();
+        // connection.disconnect(); 
+
+    }
+});
+
+// Play yoooo when someone joins
 client.on('voiceStateUpdate', async message => {
     if (message.member.voice.channel) {
-        const yoFileName = 'assets/yo.mp3';
+        const file = 'assets/yo.mp3';
         const connection = await message.member.voice.channel.join();
-        const dispatcher = connection.play(yoFileName);
+        const dispatcher = connection.play(file);
         
-        // play
-        dispatcher.on('start', () => {
-            console.log(`${yoFileName} is now playing!`);
-        });
-        
-        // when finished. Close 
-        dispatcher.on('finish', () => {
-            console.log(`${yoFileName} has finished playing!`);
-            connection.disconnect();    
-        });
-
-        // Always remember to handle errors appropriately!
-        dispatcher.on('error', console.error);
-
+        playSound(file, connection, dispatcher)
+    
     }
 });
 
